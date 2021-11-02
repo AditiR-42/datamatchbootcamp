a7cd00fffb818b0ad215a150cd611c08ad7ff397
AditiR_42 <aditisanram@gmail.com>
2021-11-01 23:51:21 -0400

    Implement CardEditor component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import CardEditor from './CardEditor';
 
-function App() {
-  return (
-    <div className="App">
-      Testing
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        {front: 'front1', back: 'back1'},
+        {front: 'front2', back: 'back2'},
+      ],
+    };
+  }
+  
+  render() {
+    return <CardEditor cards={this.state.cards}/>;
+  }
 }
 
 export default App;
